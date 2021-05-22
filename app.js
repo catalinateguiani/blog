f3342c1e483312096907ecd0f4918fb91033f0ae
Cata001 <catalinateguiani@yahoo.com>
2021-05-22 10:11:29 +0100

    Add a Database and render the posts

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const _ = require("lodash");
+const mongoose = require("mongoose");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -19,22 +19,22 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let posts = [];
+mongoose.connect("mongodb://localhost:27017/blogDB", { useNewUrlParser: true });
+
+const postSchema = {
+  title: String,
+  content: String,
+};
+
+const Post = mongoose.model("Post", postSchema);
 
 app.get("/", function (req, res) {
-  res.render("home", {
-    startingContent: homeStartingContent,
-    posts: posts,
+  Post.find({}, function (err, posts) {
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts,
+    });
   });
-  // console.log(posts);
-});
-
-app.get("/about", function (req, res) {
-  res.render("about", { startingAbout: aboutContent });
-});
-
-app.get("/contact", function (req, res) {
-  res.render("contact", { startingContact: contactContent });
 });
 
 app.get("/compose", function (req, res) {
@@ -42,30 +42,35 @@ app.get("/compose", function (req, res) {
 });
 
 app.post("/compose", function (req, res) {
-  const post = {
+  const post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody,
-  };
+  });
 
-  posts.push(post);
-
-  res.redirect("/");
-});
-
-app.get("/posts/:postName", function (req, res) {
-  //   const requestedTitle = _.lowerCase(req.params.postName);
-  const requestedTitle = req.params.postName;
-  posts.forEach(function (post) {
-    //     const storedTitle = _.lowerCase(post.title);
-    const storedTitle = post.title;
-    if (storedTitle === requestedTitle) {
-      console.log("Match found");
-      //       res.render("post", {
-      //         title: post.title,
-      //         content: post.content,
-      //       });
+  post.save(function (err) {
+    if (!err) {
+      res.redirect("/");
     }
   });
+});
+
+app.get("/posts/:postId", function (req, res) {
+  const requestedPostId = req.params.postId;
+
+  Post.findOne({ _id: requestedPostId }, function (err, post) {
+    res.render("post", {
+      title: post.title,
+      content: post.content,
+    });
+  });
+});
+
+app.get("/about", function (req, res) {
+  res.render("about", { aboutContent: aboutContent });
+});
+
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactContent: contactContent });
 });
 
 app.listen(3000, function () {
